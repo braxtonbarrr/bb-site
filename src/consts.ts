1798cb06e9989c0308ba76491a35c1a273ce7a1e
Braxton Barr <braxtonbarr2005@gmail.com>
2025-11-05 14:14:47 -0500

    Text blur

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Braxton Barr";
-export const SITE_DESCRIPTION = "Come Inside";
+export const SITE_TITLE = "braxtonbarr.ca";
+export const SITE_DESCRIPTION = "projects, ideas, unfiltered";
